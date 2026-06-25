c418f0dfdabb57d6800c032675ccc8319fe2bc04
unknown <sanjayjasti18@gmail.com>
2026-06-25 23:40:09 +0530

    Added MongoDB connection setup

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
-  res.send("Chat server is running...");
+  res.send("Chat server running...");
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
