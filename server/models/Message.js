59161542fbfda9b7dd956f206149bc3ea18d1305
unknown <sanjayjasti18@gmail.com>
2026-06-27 18:42:00 +0530

    Allow messages to store an image URL

--- server/models/Message.js
+++ server/models/Message.js
@@ -8,7 +8,11 @@ const messageSchema = new mongoose.Schema(
     },
     text: {
       type: String,
-      required: true,
+      default: "",
+    },
+    imageUrl: {
+      type: String,
+      default: "",
     },
   },
   {
